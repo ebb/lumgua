9d19f5c26e90bd4fc6a579a3c97169a49de1f4c1
Eric Bergstrome <eric.bergstrome@gmail.com>
2012-02-17 23:53:03 -0400

    Show build errors and wait for server to start.

--- lumgua.bash
+++ lumgua.bash
@@ -1,5 +1,6 @@
-bash refresh.bash >/dev/null || exit 1
+bash refresh.bash || exit 1
 ./server >server.log 2>&1 & SERVERPID=$!
+sleep 1 # XXX laziness
 ./lumgua lumgua 2>&1 | tee lumgua.log
 kill $SERVERPID
 wait
