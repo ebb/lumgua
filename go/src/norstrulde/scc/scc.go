ea8b6104973159083d23a741d70f15ddcb1cf672
Eric Bergstrome <eric.bergstrome@gmail.com>
2014-10-08 01:13:55 -0400

    Eliminate an unnecessary array

--- go/src/norstrulde/scc/scc.go
+++ go/src/norstrulde/scc/scc.go
@@ -7,28 +7,31 @@ func Compute(numVertices uint32, edges map[uint32][]uint32) [][]uint32 {
 	components := [][]uint32{}
 	stack := make([]uint32, numVertices+1)
 	index := make([]int, numVertices+1)
-	lowlink := make([]int, numVertices+1)
 	sp := 1
-	var traverse func(uint32)
-	traverse = func(m1 uint32) {
+	var traverse func(uint32) int
+	traverse = func(m1 uint32) int {
+		lowlink := sp
 		index[m1] = sp
-		lowlink[m1] = sp
 		stack[sp] = m1
 		sp++
 		for _, m2 := range edges[m1] {
+			var link int
 			if index[m2] == 0 {
-				traverse(m2)
+				link = traverse(m2)
+			} else {
+				link = index[m2]
 			}
-			if lowlink[m2] < lowlink[m1] {
-				lowlink[m1] = lowlink[m2]
+			if link < lowlink {
+				lowlink = link
 			}
 		}
-		if index[m1] == lowlink[m1] {
+		if index[m1] == lowlink {
 			c := make([]uint32, sp-index[m1])
 			copy(c, stack[index[m1]:sp])
 			sp = index[m1]
 			components = append(components, c)
 		}
+		return lowlink
 	}
 	for m := uint32(1); m <= numVertices; m++ {
 		if index[m] == 0 {
