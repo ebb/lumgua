d149dbe08b421c37c16becf13cfd7e778d1dfd70
Eric Bergstrome <eric.bergstrome@gmail.com>
2014-10-08 02:59:41 -0400

    Improve documentation and fix a type

--- go/src/norstrulde/scc/scc.go
+++ go/src/norstrulde/scc/scc.go
@@ -3,19 +3,34 @@ package scc
 // Compute the strongly-connected components of the graph. The result is
 // topologically sorted; i.e. there is no edge from a vertex in component i to
 // a vertex in component j if i<j.
+//
+// numVertices must be at least one and less than the maximum value for its
+// type. The vertices of the graph are the integers from 1 to numVertices.
+//
+// If edges[v][i] == w for some i, then there is a directed edge in the graph
+// from vertex v to vertex w.
+//
+// If there are no edges originating from a vertex v, then it is acceptable for
+// there to be no entry in the edges map for v.
+//
+// It is not required that the graph be connected.
+//
+// The set of components is a partition of the vertex set of the graph.
+//
+// This algorithm is known as Tarjan's algorithm.
 func Compute(numVertices uint32, edges map[uint32][]uint32) [][]uint32 {
 	components := [][]uint32{}
 	stack := make([]uint32, numVertices+1)
-	index := make([]int, numVertices+1)
-	sp := 1
-	var traverse func(uint32) int
-	traverse = func(m1 uint32) int {
+	index := make([]uint32, numVertices+1)
+	sp := uint32(1)
+	var traverse func(uint32) uint32
+	traverse = func(m1 uint32) uint32 {
 		lowlink := sp
 		index[m1] = sp
 		stack[sp] = m1
 		sp++
 		for _, m2 := range edges[m1] {
-			var link int
+			var link uint32
 			if index[m2] == 0 {
 				link = traverse(m2)
 			} else {
